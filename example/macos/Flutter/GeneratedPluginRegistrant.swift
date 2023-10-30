b9e50c306d8d3b1a950e71654b12f5b8f2a47698
sneurlax <sneurlax@gmail.com>
2023-10-30 10:53:56 -0500

    generated example plugin registrants

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_libsparkmobile
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLibsparkmobilePlugin.register(with: registry.registrar(forPlugin: "FlutterLibsparkmobilePlugin"))
+}
