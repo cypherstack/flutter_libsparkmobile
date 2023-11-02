bbae34d90dfbd1dd4a277211afd26ea858c8b669
sneurlax <sneurlax@gmail.com>
2023-11-02 14:39:14 -0500

    autogenerated changes
    idk if the iml changes are needed

--- test/flutter_libsparkmobile_test.dart
+++ test/flutter_libsparkmobile_test.dart
@@ -1,19 +1,19 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_libsparkmobile/flutter_libsparkmobile.dart';
-import 'package:flutter_libsparkmobile/flutter_libsparkmobile_platform_interface.dart';
 import 'package:flutter_libsparkmobile/flutter_libsparkmobile_method_channel.dart';
+import 'package:flutter_libsparkmobile/flutter_libsparkmobile_platform_interface.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockFlutterLibsparkmobilePlatform
     with MockPlatformInterfaceMixin
     implements FlutterLibsparkmobilePlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
 }
 
 void main() {
-  final FlutterLibsparkmobilePlatform initialPlatform = FlutterLibsparkmobilePlatform.instance;
+  final FlutterLibsparkmobilePlatform initialPlatform =
+      FlutterLibsparkmobilePlatform.instance;
 
   test('$MethodChannelFlutterLibsparkmobile is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelFlutterLibsparkmobile>());
@@ -21,7 +21,8 @@ void main() {
 
   test('getPlatformVersion', () async {
     FlutterLibsparkmobile flutterLibsparkmobilePlugin = FlutterLibsparkmobile();
-    MockFlutterLibsparkmobilePlatform fakePlatform = MockFlutterLibsparkmobilePlatform();
+    MockFlutterLibsparkmobilePlatform fakePlatform =
+        MockFlutterLibsparkmobilePlatform();
     FlutterLibsparkmobilePlatform.instance = fakePlatform;
 
     expect(await flutterLibsparkmobilePlugin.getPlatformVersion(), '42');
