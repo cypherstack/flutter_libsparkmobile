3bb6bc48a5ddcc496efe3a0909e11d0b73b918b0
sneurlax <sneurlax@gmail.com>
2023-11-02 14:22:54 -0500

    comments

--- scripts/linux/linkDistros.sh
+++ scripts/linux/linkDistros.sh
@@ -3,5 +3,9 @@
 WORKDIR="$(pwd)/build"
 SEDWORKDIR=$(echo $WORKDIR | sed 's/\//\\\//g')
 
+# Create new CMakelists.txt files for the sparkmobile and secp256k1 directories.
+#
+# Copy the template CMakelists.txt files to the working sparkmobile directory and replace the
+# distribution_DIR variable with the working directory.
 sed "s/SET(distribution_DIR \/opt\/android)/SET(distribution_DIR $SEDWORKDIR)/g" ./CMakeLists/sparkmobile/template_CMakeLists.txt > ./CMakeLists/sparkmobile/CMakeLists.txt
 sed "s/SET(distribution_DIR \/opt\/android)/SET(distribution_DIR $SEDWORKDIR)/g" ./CMakeLists/secp256k1/template_CMakeLists.txt > ./CMakeLists/secp256k1/CMakeLists.txt
