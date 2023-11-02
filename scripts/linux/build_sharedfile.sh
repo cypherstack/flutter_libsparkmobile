acd777d0ddb969db9ecb3dbdba610760f1dc8722
sneurlax <sneurlax@gmail.com>
2023-11-02 14:23:02 -0500

    comments

--- scripts/linux/build_sharedfile.sh
+++ scripts/linux/build_sharedfile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Git versioning.
 echo ''$(git log -1 --pretty=format:"%H")' '$(date) >> build/git_commit_version.txt
 VERSIONS_FILE=../../lib/git_versions.dart
 EXAMPLE_VERSIONS_FILE=../../lib/git_versions_example.dart
@@ -8,7 +9,11 @@ if [ ! -f "$VERSIONS_FILE" ]; then
 fi
 COMMIT=$(git log -1 --pretty=format:"%H")
 OS="LINUX"
+
+# Write the commit hash to the versions file.
 sed -i "/\/\*${OS}_VERSION/c\\/\*${OS}_VERSION\*\/ const ${OS}_VERSION = \"$COMMIT\";" $VERSIONS_FILE
+
+# Build the shared library.
 cd build
 cmake ../sparkmobile
 make -j$(nproc)
