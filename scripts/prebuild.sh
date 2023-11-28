f500f097d34f90817a2a6b63c5afdbbefe3f9c13
julian <julian@cypherstack.com>
2023-11-28 16:26:03 -0600

    add sparkmobile as submodule

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-cd ../src/deps
-
-if [ -d sparkmobile ]; then
-  rm -rf sparkmobile
-fi
-
-git clone https://github.com/firoorg/sparkmobile.git
-cd sparkmobile
-git checkout ef2e39aae18ecc49e0ddc63a3183e9764b96012e
-
 cd ..
-cp CMakeLists/sparkmobile/CMakeLists.txt     sparkmobile/
-cp CMakeLists/secp256k1/CMakeLists.txt       sparkmobile/secp256k1/
+git submodule update --init --recursive
+
+cp src/deps/CMakeLists/sparkmobile/CMakeLists.txt     src/deps/sparkmobile/
+cp src/deps/CMakeLists/secp256k1/CMakeLists.txt       src/deps/sparkmobile/secp256k1/
