b3f87d784ff4867098a1dbf7312134c2b3c3e3c5
sneurlax <sneurlax@gmail.com>
2023-12-26 16:09:23 -0600

    clean echo spam

--- scripts/windows/prep_sharedfile.sh
+++ scripts/windows/prep_sharedfile.sh
@@ -7,9 +7,6 @@ mkdir -p "$WORKDIR/sparkmobile"
 mkdir -p "$WORKDIR/sparkmobile/include"
 cp -r ../../src/deps/sparkmobile/* "$WORKDIR/sparkmobile"
 
-echo $(pwd)
-echo $SCRIPTDIR
-
 # echo ''$(git log -1 --pretty=format:"%H")' '$(date) >> build/git_commit_version.txt
 # VERSIONS_FILE=../../lib/git_versions.dart
 # EXAMPLE_VERSIONS_FILE=../../lib/git_versions_example.dart
