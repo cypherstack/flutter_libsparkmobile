56c6111b9d2e807a027e99c1a7b0065c48a95316
Julian <julian@cypherstack.com>
2024-01-03 15:31:35 -0600

    macos auto build with openssl cmake

--- scripts/macos/build_all.sh
+++ scripts/macos/build_all.sh
@@ -4,18 +4,6 @@ set -e
 
 export MACOS_SCRIPTS_DIR=`pwd`
 export PROJECT_ROOT_DIR="${MACOS_SCRIPTS_DIR}/../.."
-export MOBILE_LIB_ROOT="${PROJECT_ROOT_DIR}/src"
-export EXTERNAL_MACOS_DIR="${MOBILE_LIB_ROOT}/build/macos"
-export EXTERNAL_MACOS_SOURCE_DIR=${EXTERNAL_MACOS_DIR}/sources
-export EXTERNAL_MACOS_LIB_DIR=${EXTERNAL_MACOS_DIR}/lib
-export EXTERNAL_MACOS_INCLUDE_DIR=${EXTERNAL_MACOS_DIR}/include
-
-mkdir -p $EXTERNAL_MACOS_DIR
-mkdir -p $EXTERNAL_MACOS_LIB_DIR
-mkdir -p $EXTERNAL_MACOS_INCLUDE_DIR
-mkdir -p $EXTERNAL_MACOS_SOURCE_DIR
-
-./build_openssl_arm64.sh
 
 if [ ! -d build ] ; then
   mkdir build
--- scripts/macos/build_openssl_universal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-. ./build_openssl_common.sh
-build_openssl_universal
