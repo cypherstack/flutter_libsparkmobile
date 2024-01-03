bf738823eed6d6d52df68c04932d8c7caf231501
sneurlax <sneurlax@gmail.com>
2024-01-03 10:24:29 -0600

    remove Windows scripts
    we use CMake now.

--- scripts/windows/build_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-./build_openssl.sh
-./build_secp256k1.sh
-./prep_sharedfile.sh
--- scripts/windows/config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export SCRIPTDIR="$(pwd)/"
-export WORKDIR="$(pwd)"/../../src/build/windows
-export CACHEDIR="$(pwd)"/../../src/cache
-export SEDWORKDIR=$(echo $WORKDIR | sed 's/\//\\\//g')
-
-mkdir -p $WORKDIR
-mkdir -p $CACHEDIR
