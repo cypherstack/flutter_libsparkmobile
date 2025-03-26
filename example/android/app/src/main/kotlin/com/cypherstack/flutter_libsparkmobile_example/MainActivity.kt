9596d9d80233c8b3f0e978244b5b88f7a624c960
julian <julian@cypherstack.com>
2025-03-26 12:25:45 -0600

    gradle and ndk updates

--- example/android/app/src/main/kotlin/com/cypherstack/flutter_libsparkmobile_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/cypherstack/flutter_libsparkmobile_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.cypherstack.flutter_libsparkmobile_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
